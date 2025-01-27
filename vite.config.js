a15d5657b07d9bd1cc7ad3b54af00e71ad8abd7d
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2025-01-27 23:25:39 +0630

    feat : project setup

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+  build: {
+    lib: {
+      entry: [resolve(__dirname, "src/htmx.js")],
+      formats: ["es"],
+      name: "[name]",
+      fileName: "[name]",
+    },
+    outDir: "static",
+    emptyOutDir: false,
+  },
+});
