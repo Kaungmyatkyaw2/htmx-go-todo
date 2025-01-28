b8455285932ead6bfae0a1748ec235ba6b11bc68
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2025-01-28 23:13:21 +0630

    feat : db crud operation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Kaungmyatkyaw2/htmx-go-todo
 
 go 1.23.5
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,6 +10,21 @@ import (
 )
 
 func main() {
+
+	err := openDB()
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	defer closeDB()
+
+	err = setupDB()
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
